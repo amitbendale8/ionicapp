938e35b425274acd628c9247c2f78609f9a5ceaa
Amit <bendale.amit7@gmail.com>
2018-01-07 11:47:17 +0530

    First exercise

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Leader } from '../../providers/shared/leader';
+import { LeaderProvider } from '../../providers/leader/leader'
 
 /**
  * Generated class for the AboutPage page.
@@ -13,13 +15,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-about',
   templateUrl: 'about.html',
 })
-export class AboutPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+export class AboutPage implements OnInit{
+  leaders: Leader[];
+  errMess: string;
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+            private leaderProvider: LeaderProvider,
+            @Inject('BaseURL') private BaseURL) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AboutPage');
+  }
+
+  ngOnInit(){
+    console.log("On Init Started");
+    this.leaderProvider.getLeaders().subscribe
+    (leaders => {console.log("function called");
+                console.log(leaders.length);
+      this.leaders = leaders},
+    errMess => this.errMess = errMess
+    )
   }
 
 }
